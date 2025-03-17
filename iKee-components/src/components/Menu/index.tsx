9d8883c7ec5a94fe6ab4b91807777f81c89ca265
邓爱琪 <magic_8@qq.com>
2025-03-17 23:07:38 +0800

    ikee-component 1.0.5

---
+++ iKee-components/src/components/Menu/index.tsx
@@ -0,0 +1,9 @@
+import Menu from "./Menu";
+import MenuItem from "./MenuItem";
+import Submenu from "./Submenu";
+
+// export { Menu, MenuItem, Submenu };
+// export default {Menu,MenuItem,Submenu}
+export default Menu;
+// export default MenuItem
+// export default Submenu
--- iKee-components/src/index.ts
+++ iKee-components/src/index.ts
@@ -1,1 +1,6 @@
-export  {default as Button}  from "./components/Button"
+export {default as Button } from './components/Button'
+export {default as Input } from './components/Input'
+export {default as Autocomplete } from './components/Autocomplete'
+export {default as Menu } from './components/Menu'
+// export { default as MenuItem } from './components/MenuItem';
+// export { default as Submenu } from './components/Submuenu';
